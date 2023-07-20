e5dbd18f23f4d323b5f94e8b7b7b64e8e8a2b6c9
anthony <zengkun@netfarmer.com.cn>
2023-07-20 16:43:48 +0800

    增加微信客服接口

--- lib/wework/api/methods/kf.rb
+++ lib/wework/api/methods/kf.rb
@@ -15,6 +15,14 @@ module Wework
           })
         end
 
+        def send_msg_on_event(code:, msgid: nil, msgtype:)
+          post('/kf/send_msg_on_event', {
+            code: code,
+            msgid: msgid,
+            msgtype: msgtype
+          })
+        end
+
         def account_list(offset: 0, limit: 50)
           post('/kf/account/list', {
             offset: offset,
@@ -36,11 +44,63 @@ module Wework
           })
         end
 
-        def servicer_add(userid_list, department_id_list, open_kfid:)
+        def servicer_add(userid_list:, department_id_list:, open_kfid:)
           post('/kf/servicer/add', {
             userid_list: userid_list,
             department_id_list: department_id_list,
             open_kfid: open_kfid
+          })
+        end
+
+        def servicer_del(userid_list:, department_id_list:, open_kfid:)
+          post('/kf/servicer/del', {
+            userid_list: userid_list,
+            department_id_list: department_id_list,
+            open_kfid: open_kfid
+          })
+        end
+
+        def servicer_list(open_kf_id)
+          get '/kf/servicer/list', params: { open_kfid: open_kf_id }
+        end
+
+        ##
+        # 分配客服会话
+        #
+        def service_state_get(open_kfid:, external_userid:)
+          post('/kf/service_state/get', {
+            open_kfid: open_kfid,
+            external_userid: external_userid
+          })
+        end
+
+        def service_state_trans(open_kfid:, external_userid:, service_state:, servicer_userid: nil)
+          post('/kf/service_state/trans', {
+            open_kfid: open_kfid,
+            external_userid: external_userid,
+            service_state: service_state,
+            servicer_userid: servicer_userid
+          })
+        end
+
+        ##
+        # 统计
+        #
+
+        def get_corp_statistic(open_kfid:, start_time:, end_time:)
+          post('/kf/get_corp_statistic', {
+            open_kfid: open_kfid,
+            start_time: start_time,
+            end_time: end_time
+          })
+        end
+
+        def get_servicer_statistic(open_kfid:, servicer_userid:, start_time:, end_time:)
+          post('/kf/get_servicer_statistic', {
+            open_kfid: open_kfid,
+            servicer_userid: servicer_userid,
+            start_time: start_time,
+            end_time: end_time
           })
         end
 
