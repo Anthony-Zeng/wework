e53a8e62f6341e86e88ca05185cfc7e48b6dee82
anthony <zengkun@netfarmer.com.cn>
2023-11-09 13:47:24 +0800

    feat:

--- lib/wework/api/methods/provider.rb
+++ lib/wework/api/methods/provider.rb
@@ -2,21 +2,21 @@ module Wework
   module Api
     module Methods
       module Provider
-        def sso_authorize_url(redirect_uri, user_type='admin', state='qywxlogin')
+        def sso_authorize_url(redirect_uri, user_type = 'admin', state = 'qywxlogin')
           uri = ERB::Util.url_encode(redirect_uri)
           "#{SSO_AUTHORIZE_ENDPOINT}?appid=#{corp_id}&redirect_uri=#{uri}&state=#{state}&usertype=#{user_type}"
         end
 
         def get_login_info auth_code
-          post 'service/get_login_info', {auth_code: auth_code, access_token: access_token}
+          post 'service/get_login_info', { auth_code: auth_code, access_token: access_token }
         end
 
-        def get_register_code template_id, options={}
-          params = {template_id: template_id}
+        def get_register_code template_id, options = {}
+          params = { template_id: template_id }
           post 'service/get_register_code', params.merge(options)
         end
 
-        def get_register_url template_id, options={}
+        def get_register_url template_id, options = {}
           register_code = get_register_code(template_id, options).register_code
           "#{REGISTER_ENDPOINT}?register_code=#{register_code}"
         end
@@ -26,11 +26,18 @@ module Wework
         end
 
         def id_translate auth_corpid, media_id
-          post 'service/contact/id_translate', { auth_corpid: auth_corpid, media_id_list: [ media_id ] }
+          post 'service/contact/id_translate', { auth_corpid: auth_corpid, media_id_list: [media_id] }
         end
 
         def service_batch_getresult job_id
           get 'service/batch/getresult', params: { jobid: job_id }
+        end
+
+        def get_customized_auth_url(template_id_list, state)
+          post 'service/get_customized_auth_url', {
+            templateid_list: template_id_list,
+            state: state
+          }
         end
       end
     end
--- lib/wework/version.rb
+++ lib/wework/version.rb
@@ -1,3 +1,3 @@
 module Wework
-  VERSION = '1.3.9.1'.freeze
+  VERSION = '1.3.9.2'.freeze
 end
